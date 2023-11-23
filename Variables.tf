1557dad89ace681e15581385cdfa58432fd78d9f
Naih2021 <munthaliesnai@gmail.com>
2023-11-22 22:37:34 -0500

    dockerlab module

---
+++ Variables.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "default_vpc" {
+  default = ""
+
+}
+variable "security_group" {
+  default = "docker-Web-SG"
+}
+
+variable "from_port1" {
+  default = "8000"
+}
+
+variable "from_port" {
+  default = "22"
+}
+
+variable "to_port1" {
+  default = "8100"
+}
+
+variable "to_port" {
+  default =  "22"
+}
+
+
+variable "key_name" {
+  default = "privatekeypair"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  default = "docker instance"
+}
+
